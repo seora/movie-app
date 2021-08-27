69c608c87d05001023c473b542f7e55b4ef6df9c
seora <tjfk4938@hanmail.net>
2021-08-28 05:03:57 +0900

    get movie json and render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -7,22 +9,48 @@ class App extends React.Component{
     movies: []
   };
 
+
+  //json 가져오는데 시간이 걸리기 때문에 비동기로 await 활용
+  getMovies = async() => {
+    const {data : {data : {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies, isLoading : false});
+  }
+
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000)
+    this.getMovies();
   }
 
   //setstate를 호출할 때마다 react는 새로운 state와 함께 render 호출
 
   render(){
 
-    const{ isLoading } = this.state;
+    const{ isLoading, movies } = this.state;
 
     return(
-      <div>
-        {isLoading? "Loading..." : "We are ready"}
-      </div>
+      <section className = "container">
+        {isLoading? 
+          (<div className = "loader">
+            <span className="loader_text">"Loading..."</span>
+          </div>)
+          : (
+            <div className = "movies">
+              {
+                movies.map(movie => (
+                  <Movie 
+                  key = {movie.id}
+                  id = {movie.id} 
+                  year = {movie.year} 
+                  title = {movie.title} 
+                  summary = {movie.summary}
+                  poster = {movie.medium_cover_image}
+                  genres = {movie.genres}
+                  />
+                ))
+              }
+            </div>
+          )}
+      </section>
     );
   }
 }
